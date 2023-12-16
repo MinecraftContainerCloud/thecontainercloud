c19c4062cce4694ebace3b04274a3db3e8698df0
shorty <ungegrieftfirma@gmail.com>
2023-12-16 21:29:12 +0100

    feat(add services)

--- src/main/java/de/theccloud/thecontainercloud/Main.java
+++ src/main/java/de/theccloud/thecontainercloud/Main.java
@@ -2,6 +2,7 @@ package de.theccloud.thecontainercloud;
 
 import de.theccloud.thecontainercloud.communication.web.WebCommunicationManager;
 import de.theccloud.thecontainercloud.database.DatabaseInteractionHandler;
+import de.theccloud.thecontainercloud.database.service.ServiceTable;
 import de.theccloud.thecontainercloud.database.tasks.TaskTable;
 
 public class Main {
@@ -10,8 +11,9 @@ public class Main {
 
         DatabaseInteractionHandler databaseInteractionHandler = new DatabaseInteractionHandler();
         TaskTable taskTable = new TaskTable(databaseInteractionHandler);
+        ServiceTable serviceTable = new ServiceTable(databaseInteractionHandler);
 
-        new WebCommunicationManager(taskTable);
+        new WebCommunicationManager(taskTable, serviceTable);
 
     }
 
